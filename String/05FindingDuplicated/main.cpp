8ddb5dc96e308d98966aa82f8141d1c675ff7c0a
MarioTiara <mario.tiarapratama@gmail.com>
2022-09-10 21:07:15 +0700

    finding duplicate using bitwise

--- String/05FindingDuplicated/main.cpp
+++ String/05FindingDuplicated/main.cpp
@@ -17,9 +17,24 @@ void findDuplicateUsingHash(){
         }
     }
 }
-int main(){
 
+void FindingDuplicateByBiteWise(){
+        //findDuplicateUsingHash();
+    char A[] ="findings";
+    int H =0, x=0;
+    for (int i=0; A[i]!='\0'; i++){
+        x=1;
+        x=x<<(A[i]-97);
+        if ((x&H)>0){
+            cout<<A[i]<<" is duplicate"<<endl;
+        }else {
+            H=x|H;
+        }
+    }
+}
+int main(){
     findDuplicateUsingHash();
+    FindingDuplicateByBiteWise();
 
   
     return 0;
