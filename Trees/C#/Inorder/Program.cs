8b83de67b08bdc8f8908f047ddbe6895a28c6bd7
MarioTiara <mario.tiarapratama@gmail.com>
2022-10-09 21:19:11 +0700

    Init Inorder Tree

---
+++ Trees/C#/Inorder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inorder
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Node{
+        int data;
+        Node right, left;
+
+        public Node (int key){
+            this.data=key;
+        }
+
+        public void insert (int key){
+            if (key<=data){
+                if (right==null){
+                    right=new Node(key);
+                }else{
+                    right.insert(key);
+                }
+            }else{
+                if (left==null){
+                    left=new Node(key);
+                }else{
+                    left.insert(key);
+                }
+            }
+        }
+    }
+}
