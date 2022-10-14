06c2e0d4c6fdfcd1f14a1482594065d2a78389b7
MarioTiara <mario.tiarapratama@gmail.com>
2022-10-14 09:29:33 +0700

    Min Palindrome updte

--- Algorithm/Search/Program.cs
+++ Algorithm/Search/Program.cs
@@ -15,19 +15,31 @@ namespace Search
         public static int MinimumInsertTobePalindrome(string input){
             HashSet<char> already= new HashSet<char>();
             int minNumber=0;
+            int counter=0;
             foreach(var chr in input){  
                 if (!already.Contains(chr)){
                     already.Add(chr);
                     char [] charinArry= input.Where(c=>c==chr).ToArray();
+                     if (charinArry.Count()>2){
+                        counter+=1;
+                     }
                      if ((charinArry.Count()%2)!=0){
                             minNumber+=1;
                     } 
                 }
                
             }
-            if (input.Length%2!=0) return minNumber-1;
 
+  
+
+            if ((counter>=1) && (input.Length%2==0)){
+                return minNumber-1;
+            }else if ((input.Length%2!=0) && counter<=1){
+                return minNumber-1;
+            }
             return minNumber;
+            
+            
         }
 
         
