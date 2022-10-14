5d12e1ff066606f93d901d14b37ef735053feca4
MarioTiara <mario.tiarapratama@gmail.com>
2022-10-14 08:12:08 +0700

    Min Palindrome

---
+++ Algorithm/Search/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min=MinimumInsertTobePalindrome("geeks");
+            Console.WriteLine(min);
+        }
+
+
+        public static int MinimumInsertTobePalindrome(string input){
+            HashSet<char> already= new HashSet<char>();
+            int minNumber=0;
+            foreach(var chr in input){  
+                if (!already.Contains(chr)){
+                    already.Add(chr);
+                    char [] charinArry= input.Where(c=>c==chr).ToArray();
+                     if ((charinArry.Count()%2)!=0){
+                            minNumber+=1;
+                    } 
+                }
+               
+            }
+            if (input.Length%2!=0) return minNumber-1;
+
+            return minNumber;
+        }
+
+        
+    }
+}
