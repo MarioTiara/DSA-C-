fd6c1fa37af7481eb75ce74dd23ac2c234f74fa7
MarioTiara <mario.tiarapratama@gmail.com>
2022-10-24 09:39:11 +0700

    Insert Node At End

--- LinkedList/LinkedListOperation/main.cpp
+++ LinkedList/LinkedListOperation/main.cpp
@@ -10,6 +10,7 @@ class Node {
 
 class LinkedList {
     public:
+        int lenght=0;;
         Node * head, * tail;
         LinkedList(){
             head=NULL;
@@ -22,9 +23,40 @@ class LinkedList {
         if (head==NULL){
             head=newNode;
             tail= newNode;
+            
         }else{
             newNode->next=head;
             head=newNode;
         }
+        lenght++;
     }
+
+    void insertNodeAtEnd(int value){
+        Node * newNode = new Node;
+        newNode->data=value;
+        newNode->next=NULL;
+        if (head==NULL){
+            head= newNode;
+            tail=head;
+            return;
+        }else{
+            tail->next=newNode;
+            tail=tail->next;
+            return;
+        }
+        lenght++;
+    }
+
+    
+};
+
+int main(){
+    LinkedList list;
+    list.insertNodeAtFront(40);
+    list.insertNodeAtFront(30);
+    list.insertNodeAtFront(20);
+    list.insertNodeAtFront(10);
+    list.insertNodeAtFront(21);
+    cout<<list.lenght<<endl;
+    return 0;
 }
