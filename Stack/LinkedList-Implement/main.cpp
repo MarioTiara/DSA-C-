e7a22ff7918c684f9ae945b2515076b4efce9f59
MarioTiara <mario.tiarapratama@gmail.com>
2022-10-27 13:44:22 +0700

    [Stack] LinkedList implementation - pop ()

--- Stack/LinkedList-Implement/main.cpp
+++ Stack/LinkedList-Implement/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Node {
         public:
-            int dat;
+            int data;
             Node *next;
 };
 
@@ -14,11 +14,46 @@ class Stack{
                 top=NULL;
             }
         void push(int data);
-        void pop();
+        int pop();
         int peek();
         void display();
+};
+
+void Stack::push(int data){
+    Node * newNode= new Node();
+    newNode->data=data;
+    if (top==NULL){
+        newNode->next=NULL;
+    }else{
+        newNode->next=top;
+    }
+    top=newNode;
+
+    
+}
+
+int Stack::pop(){
+    Node * temp;
+    if (top==NULL){
+        cout<<"Stack underflow"<<endl;
+        return -1;
+    }
+    temp=top;
+    top=top->next;
+    int data=temp->data;
+    delete temp;
+    return data;
+    
+    
 }
 
 int main(){
+    Stack stack;
+    stack.push(23);
+    stack.push(21);
+    cout<<stack.pop()<<endl;
+    cout<<stack.pop()<<endl;
+    cout<<stack.pop()<<endl;
+
     return 0;
 }
