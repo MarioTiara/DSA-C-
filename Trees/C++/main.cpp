d48477a933bf1d95dde7e42789f317e668100936
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-01 20:55:51 +0700

    [Trees] createNewNode

--- Trees/C++/main.cpp
+++ Trees/C++/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 
 class TreeNode {
-    int data;
-    TreeNode *left, *right;
+    public:
+        int data;
+        TreeNode *left, *right;
+    TreeNode createNewNode(int value);
+};
+
+TreeNode TreeNode::createNewNode(int value){
+    TreeNode * newNode = new TreeNode;
+    newNode->data=value;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
 }
+
+int main(){
+
+    return 0;
+}
+
